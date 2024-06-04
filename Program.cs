0fd5dc364228938767351e84b5552d46f85f69d3
Tshikwetamakole <105504810+Tshikwetamakole@users.noreply.github.com>
2024-06-04 15:01:28 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MyNamespace 
+{
+    internal class Program
+    {
+        double length;
+        double width;
+
+        public Program(double l, double w)
+        {
+            length = l;
+            width = w;
+        }
+
+        public double GetArea()
+        {
+            return length * width;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Length: " + length);
+            Console.WriteLine("Width: " + width);
+            Console.WriteLine("Area: " + GetArea());
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Length:");
+            double length = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Width:");
+            double width = double.Parse(Console.ReadLine());
+
+            Program program = new Program(length, width);
+            program.Display();
+        }
+    }
+}
